4b6e8953a5ef2263878fe697f918147b6273f5a0
narendiransankar <narendiransankar1699@gmail.com>
2025-07-18 00:51:46 +0530

    Update main.tf

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -29,7 +29,7 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = var.stage_name
   triggers = {
-      redeployment = timestamp()
+      redeploy = timestamp()
     }
 }
 
