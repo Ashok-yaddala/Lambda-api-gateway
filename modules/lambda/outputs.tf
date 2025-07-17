cd018508f1668ae846895b45d89fc48494047a20
narendiransankar <narendiransankar1699@gmail.com>
2025-07-17 23:49:12 +0530

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,3 +1,6 @@
 output "lambda_arn" { 
-value = aws_lambda_function.this.arn 
+  value = aws_lambda_function.this.arn 
+}
+output "xray_enabled" {
+  value = aws_lambda_function.this.tracing_config[0].mode
 }
