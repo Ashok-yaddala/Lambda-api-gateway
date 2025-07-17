41d09fe5c7731b0fb795bd7a78700e0291d31658
narendiransankar <narendiransankar1699@gmail.com>
2025-07-18 00:42:37 +0530

    Update main.tf

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -28,6 +28,9 @@ resource "aws_api_gateway_deployment" "this" {
   depends_on  = [aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = var.stage_name
+  triggers = {
+      redeployment = timestamp()
+    }
 }
 
 resource "aws_lambda_permission" "apigw" {
