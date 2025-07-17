25083ae87b88d9f18470aa1404af638f9d7f0fe8
narendiransankar <narendiransankar1699@gmail.com>
2025-07-18 00:03:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
-variable "aws_region"     { 
+variable "aws_region" { 
   default = "ap-south-1" 
 }
 variable "repository_name" { 
   default = "hello-lambda" 
 }
-variable "lambda_name"    { 
+variable "lambda_name" { 
   default = "hello-docker-lambda" 
 }
-variable "image_uri"      { 
-  description = "ECR image URI with tag (set manually after push)" 
+variable "image_uri" { 
+  description = "ECR image URI with tag (updated by CI/CD)" 
 }
-variable "api_name"       { 
+variable "api_name" { 
   default = "docker-python" 
 }
-variable "stage_name"     { 
+variable "stage_name" { 
   default = "test" 
 }
