d491cc1a0c222034e588ec7206b94c35bb61aa08
narendiransankar <narendiransankar1699@gmail.com>
2025-07-17 23:11:39 +0530

    Create main.tf

---
+++ modules/lambda/main.tf
@@ -0,0 +1,9 @@
+resource "aws_lambda_function" "this" {
+  function_name = var.lambda_name
+  package_type  = "Image"
+  image_uri     = var.image_uri
+  role          = var.role_arn
+  timeout       = 10
+}
+
+
