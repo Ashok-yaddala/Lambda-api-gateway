f77e02b3881540434e51813f319c22415bf3a765
narendiransankar <narendiransankar1699@gmail.com>
2025-07-17 23:59:22 +0530

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,6 +1,9 @@
-output "lambda_arn" { 
-  value = aws_lambda_function.this.arn 
+output "lambda_arn" {
+  value = aws_lambda_function.this.arn
 }
-output "xray_enabled" {
-  value = aws_lambda_function.this.tracing_config[0].mode
+output "lambda_name" {
+  value = aws_lambda_function.this.function_name
+}
+output "role_arn" {
+  value = aws_iam_role.lambda_exec.arn
 }
