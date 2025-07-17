0785e1fea79e660b4e456698af2ff7c114897015
narendiransankar <narendiransankar1699@gmail.com>
2025-07-18 00:21:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "lambda" {
 module "apigateway" {
   source             = "./modules/apigateway"
   api_name           = var.api_name
+  region             = var.aws_region
   lambda_invoke_arn  = module.lambda.lambda_arn
   lambda_name        = module.lambda.lambda_name
   stage_name         = var.stage_name
