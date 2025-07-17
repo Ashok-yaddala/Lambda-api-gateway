831da1507c8f33fd2dc61a1ae70c2571be557575
narendiransankar <narendiransankar1699@gmail.com>
2025-07-18 00:00:10 +0530

    Update main.tf

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_integration" "lambda" {
 }
 
 resource "aws_api_gateway_deployment" "this" {
-  depends_on = [aws_api_gateway_integration.lambda]
+  depends_on  = [aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = var.stage_name
 }
@@ -33,9 +33,11 @@ resource "aws_api_gateway_deployment" "this" {
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
+  function_name = var.lambda_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/*"
 }
 
-
+output "invoke_url" {
+  value = "${aws_api_gateway_rest_api.this.execution_arn}/${var.stage_name}/hello"
+}
